a8fd2e181b6442fed69c2a4f9ee23cb645d8f0f3
Marcel Wang <mrcel.wng@gmail.com>
2026-04-08 16:50:43 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="w-full h-screen flex">
+      <h1 className="text-xl garet font-bold">
+        <span className="text-gray-400">stand</span>rom
+      </h1>
+    </div>
+  );
+}
